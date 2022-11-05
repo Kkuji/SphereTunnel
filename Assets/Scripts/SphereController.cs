39ce0a91745d3b8ad455a87582e44cba7a46b637
Kkuji <99590492+Kkuji@users.noreply.github.com>
2022-11-05 19:01:51 +0300

    FirstSession

--- Assets/Scripts/SphereController.cs
+++ Assets/Scripts/SphereController.cs
@@ -5,12 +5,14 @@ public class SphereController : MonoBehaviour
     private bool timeEnded = true;
     private float verticalSpeed = 0.01f;
     public static bool dead = false;
+    
     void Update()
     {
         if (!dead)
         {
             if (Input.GetKey(KeyCode.Space) && transform.position.y < 7)
                 transform.position = new Vector3(transform.position.x, transform.position.y + verticalSpeed, transform.position.z);
+                
             if (!Input.GetKey(KeyCode.Space) && transform.position.y > 0.2)
                 transform.position = new Vector3(transform.position.x, transform.position.y - verticalSpeed, transform.position.z);
 
@@ -21,6 +23,7 @@ public class SphereController : MonoBehaviour
             }
         }
     }
+    
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Line"))
